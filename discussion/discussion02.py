66c47b6c3b1f3c06e9d1fbefaa6bd764562459ce
bitterlucky <925182053@qq.com>
2025-05-07 15:14:27 +0800

    discussion02

---
+++ discussion/discussion02.py
@@ -0,0 +1,63 @@
+def make_keeper(n):
+    """Returns a function that takes one parameter cond and prints
+    out all integers 1..i..n where calling cond(i) returns True.
+
+    >>> def is_even(x): # Even numbers have remainder 0 when divided by 2.
+    ...     return x % 2 == 0
+    >>> make_keeper(5)(is_even)
+    2
+    4
+    >>> make_keeper(5)(lambda x: True)
+    1
+    2
+    3
+    4
+    5
+    >>> make_keeper(5)(lambda x: False)  # Nothing is printed
+    """
+    def f(cond):
+        for i in range(1, n + 1):
+            if cond(i):
+                print(i)
+    return f
+
+def find_digit(k):
+    """Returns a function that returns the kth digit of x.
+
+    >>> find_digit(2)(3456)
+    5
+    >>> find_digit(2)(5678)
+    7
+    >>> find_digit(1)(10)
+    0
+    >>> find_digit(4)(789)
+    0
+    """
+    assert k > 0
+    return lambda x : x // (10 ** (k - 1)) % 10
+
+def match_k(k):
+    """Returns a function that checks if digits k apart match.
+
+    >>> match_k(2)(1010)
+    True
+    >>> match_k(2)(2010)
+    False
+    >>> match_k(1)(1010)
+    False
+    >>> match_k(1)(1)
+    True
+    >>> match_k(1)(2111111111111111)
+    False
+    >>> match_k(3)(123123)
+    True
+    >>> match_k(2)(123123)
+    False
+    """
+    def check(x):
+        while x // (10 ** k) > 0:
+            if x % 10 != x // (10 ** k) % 10:
+                return False
+            x //= 10
+        return True
+    return check
