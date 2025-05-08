ad3208e7910ef271309490903b822f75003a7be6
bitterlucky <925182053@qq.com>
2025-05-08 15:30:11 +0800

    discussion03

---
+++ discussion/discussion03.py
@@ -0,0 +1,83 @@
+def swipe(n):
+    """Print the digits of n, one per line, first backward then forward.
+
+    >>> swipe(2837)
+    7
+    3
+    8
+    2
+    8
+    3
+    7
+    """
+    if n < 10:
+        print(n)
+    else:
+        print(n % 10)
+        swipe(n // 10)
+        print(n % 10)
+
+def skip_factorial(n):
+    """Return the product of positive integers n * (n - 2) * (n - 4) * ...
+
+    >>> skip_factorial(5) # 5 * 3 * 1
+    15
+    >>> skip_factorial(8) # 8 * 6 * 4 * 2
+    384
+    """
+    if n <= 2:
+        return n
+    else:
+        return n * skip_factorial(n - 2)
+    
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    # helper function is for to recursively implement is_prime because it has a variable called i,it
+    # computes for i in range(2, n - 1) to see if n is_prime
+    def helper(i):
+        if i == n:
+            return True
+        if n % i == 0:
+            return False
+        else:
+            return helper(i + 1)
+    return helper(2)
+
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, 
+    and return the number of elements in the sequence.
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    print(n)
+    if n % 2 == 0:
+        return even(n)
+    else:
+        return odd(n)
+
+def even(n):
+    return 1 + hailstone(n // 2)
+
+def odd(n):
+    if n == 1:
+        return 1
+    return 1 + hailstone(n * 3 + 1)
