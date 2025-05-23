fc15369728e15aead8be48c71c856fb694b7bb5a
bitterlucky <925182053@qq.com>
2025-05-23 20:29:55 +0800

    discussion07

---
+++ discussion/discussion07.py
@@ -0,0 +1,140 @@
+def draw(hand, positions):
+    """Remove and return the items at positions from hand.
+
+    >>> hand = ['A', 'K', 'Q', 'J', 10, 9]
+    >>> draw(hand, [2, 1, 4])
+    ['K', 'Q', 10]
+    >>> hand
+    ['A', 'J', 9]
+    """
+    return list(reversed( [hand.pop(i) for i in reversed(sorted(positions))] ))
+
+class Eye:
+    """An eye.
+
+    >>> Eye().draw()
+    '0'
+    >>> print(Eye(False).draw(), Eye(True).draw())
+    0 -
+    """
+    def __init__(self, closed=False):
+        self.closed = closed
+
+    def draw(self):
+        if self.closed:
+            return '-'
+        else:
+            return '0'
+
+class Bear:
+    """A bear.
+
+    >>> Bear().print()
+    ? 0o0?
+    """
+    def __init__(self):
+        self.nose_and_mouth = 'o'
+
+    def next_eye(self):
+        return Eye()
+
+    def print(self):
+        left, right = self.next_eye(), self.next_eye()
+        print('? ' + left.draw() + self.nose_and_mouth + right.draw() + '?')
+
+class SleepyBear(Bear):
+    """A bear with closed eyes.
+
+    >>> SleepyBear().print()
+    ? -o-?
+    """
+    def next_eye(self):
+        return Eye(True)
+
+class WinkingBear(Bear):
+    """A bear whose left eye is different from its right eye.
+
+    >>> WinkingBear().print()
+    ? -o0?
+    """
+    def __init__(self):
+        super().__init__()
+        self.eye_calls = 0
+
+    def next_eye(self):
+        self.eye_calls += 1
+        return Eye(self.eye_calls % 2)
+
+
+LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+
+class CapsLock:
+    def __init__(self):
+        self.pressed = 0
+
+    def press(self):
+        self.pressed += 1
+
+class Button:
+    """A button on a keyboard.
+
+    >>> f = lambda c: print(c, end='')  # The end='' argument avoids going to a new line
+    >>> k, e, y = Button('k', f), Button('e', f), Button('y', f)
+    >>> s = e.press().press().press()
+    eee
+    >>> caps = Button.caps_lock
+    >>> t = [x.press() for x in [k, e, y, caps, e, e, k, caps, e, y, e, caps, y, e, e]]
+    keyEEKeyeYEE
+    >>> u = Button('a', print).press().press().press()
+    A
+    A
+    A
+    """
+    caps_lock = CapsLock()
+
+    def __init__(self, letter, output):
+        assert letter in LOWERCASE_LETTERS
+        self.letter = letter
+        self.output = output
+        self.pressed = 0
+
+    def press(self):
+        """Call output on letter (maybe uppercased), then return the button that was pressed."""
+        self.pressed += 1
+        if self.caps_lock.pressed % 2 == 0:
+            self.output(self.letter)
+        else:
+            self.output(self.letter.upper())
+        return self
+    
+
+class Keyboard:
+    """A keyboard.
+
+    >>> Button.caps_lock.pressed = 0  # Reset the caps_lock key
+    >>> bored = Keyboard()
+    >>> bored.type('hello')
+    >>> bored.typed
+    ['h', 'e', 'l', 'l', 'o']
+    >>> bored.keys['l'].pressed
+    2
+
+    >>> Button.caps_lock.press()
+    >>> bored.type('hello')
+    >>> bored.typed
+    ['h', 'e', 'l', 'l', 'o', 'H', 'E', 'L', 'L', 'O']
+    >>> bored.keys['l'].pressed
+    4
+    """
+    def __init__(self):
+        self.typed = []
+        self.keys = {}  # Try a dictionary comprehension!
+
+    def type(self, word):
+        """Press the button for each letter in word."""
+        assert all([w in LOWERCASE_LETTERS for w in word]), 'word must be all lowercase'
+        for w in word:
+            if w not in self.keys:
+                self.keys[w] = Button(w, lambda x: self.typed.append(x))
+            letter = self.keys[w].press()
+        
