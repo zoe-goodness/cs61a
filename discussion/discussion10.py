c2d76f58914bdbd9f90713ab43fc6d14951b70bf
bitterlucky <925182053@qq.com>
2025-06-04 13:26:13 +0800

    discussion10

---
+++ discussion/discussion10.py
@@ -0,0 +1,36 @@
+def print_evals(expr):
+        """Print the expressions that are evaluated while evaluating expr.
+
+        expr: a Scheme expression containing only (, ), +, *, and numbers.
+
+        >>> nested_expr = Pair('+', Pair(Pair('*', Pair(3, Pair(4, nil))), Pair(5, nil)))
+        >>> print_evals(nested_expr)
+        (+ (* 3 4) 5)
+        +
+        (* 3 4)
+        *
+        3
+        4
+        5
+        >>> print_evals(Pair('*', Pair(6, Pair(7, Pair(nested_expr, Pair(8, nil))))))
+        (* 6 7 (+ (* 3 4) 5) 8)
+        *
+        6
+        7
+        (+ (* 3 4) 5)
+        +
+        (* 3 4)
+        *
+        3
+        4
+        5
+        8
+        """
+        if not isinstance(expr, Pair):
+            print(expr)
+        else:
+            print(expr)
+            temp = expr
+            while temp is not nil:
+                 print_evals(temp.first)
+                 temp = temp.rest
