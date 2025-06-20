2c5b3e57c3bc80aa741a9960db1f2ad29aec0aee
bitterlucky <925182053@qq.com>
2025-06-20 20:39:30 +0800

    discussion11 everything done.Contragulations

--- discussion/discussion11.scm
+++ discussion/discussion11.scm
@@ -1,20 +1,26 @@
+; question 1 
+(define (mystery expr old new)
+    (mystery-helper expr old new))
+
+(define-macro (mystery-macro expr old new)
+    (mystery-helper expr old new))
+
+(define (mystery-helper e o n)
+  (if (pair? e)
+      (cons (mystery-helper (car e) o n) (mystery-helper (cdr e) o n))
+      (if (eq? e o) n e)))
+; question 2
+(define-macro (assign sym1 sym2 expr1 expr2)
+  `(begin
+     (define ,sym1 ,expr1)
+     (define ,sym2 ,(eval expr2))))
+
+; question 3
 (define-macro (switch expr cases)
     `(let ((val ,expr))
 	  ,(cons
-	    cond
+	    `cond
 	    (map (lambda (case) (cons
-	           (list equal? val (car case))
+	           (list `equal? `val (car case))
 		       (cdr case)))
 		     cases))))
-
-
-(defin (switch expr cases)
-    `(let ((val ,expr))
-	  ,(cons
-	    cond
-	    (map (lambda (case) (cons
-	           (list equal? val (car case))
-		       (cdr case)))
-		     cases))))
-
-(switch (+ 1 1) ((1 (print 'a))(2 (print 'b))(3 (print 'c))))
